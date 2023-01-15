c9c3f01f81fce30329ea602129540737fd236d2d
Mike Zhao <yzhao@upgrade.com>
2023-01-15 07:47:43 -0500

    first commit

---
+++ senior/s1_1.py
@@ -0,0 +1,48 @@
+import glob
+import time
+import os
+
+path = '/Users/yzhao/ccc/2022/senior/s1/'
+
+def find_ways1(num):
+    count = 0
+    for i in range(num // 4 + 1):
+        for j in range(num // 5 + 1):
+            if i * 4 + j * 5 == num:
+                count += 1
+    return count
+
+
+def find_ways2(num):
+    count = 0
+    for i in range(num // 4 + 1):
+        j = (num - 4*i)//5
+        if i*4+j*5 == num:
+            count += 1
+    return count
+
+
+files = glob.glob("/Users/yzhao/ccc/2022/senior/s1/s1.*.in")
+files.sort(key=os.path.getmtime)
+for file_name in files:
+    with open(file_name, "r") as file:
+        for line in file:
+            print("working on {}".format(file_name))
+            # start1 = time.time()
+            # res1 = find_ways1(int(line))
+            # time1 = time.time() - start1
+            # print(file_name)
+            # print("way1 took {} sec".format(time1))
+            start2 = time.time()
+            res2 = find_ways2(int(line))
+            time2 = time.time() - start2
+            print("way2 took {} sec".format(time2))
+            expected = -1
+            with open(file_name.replace("in", "out")) as ofile:
+                expected = int(ofile.readline())
+            # if res1 != expected:
+            #     print("way1 wrong answer, expected {}, result {}".format(
+            #         expected, res1))
+            if res2 != expected:
+                print("way1 wrong answer, expected {}, result {}".format(
+                    expected, res2))
